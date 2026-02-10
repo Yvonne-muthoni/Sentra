13c496b2019f2d494c284285add21b6f49e6e5e1
Yvonne Muriuki <yvonnemuriuki01@gmail.com>
2026-02-10 10:26:27 +0300

    final commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,12 @@
 export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}'
+  ],
+  safelist: [
+    'font-opensans',
+    'font-montserrat'
+  ],
   theme: {
     extend: {
       fontFamily: {
